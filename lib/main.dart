7db9d2721322168f9019fad080890b071ac935b8
saivishnu725 <saivishnu725@gmail.com>
2021-05-11 18:58:52 +0530

    Made lib/main.dart look like Layout-Challenge.png

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Container with a Text"),
-        backgroundColor: Colors.grey,
-        elevation: 10.0,
-        centerTitle: true,
-      ),
+      // appBar: AppBar(
+      //   title: Text("Container with a Text"),
+      //   backgroundColor: Colors.grey,
+      //   elevation: 10.0,
+      //   centerTitle: true,
+      // ),
       backgroundColor: Colors.teal,
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Container(
-          color: Colors.white,
-          child: Text("Hello!!"),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100.0,
+            ),
+            // SizedBox(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.yellow,
+                  width: 100.0,
+                  height: 100.0,
+                ),
+                Container(
+                  color: Colors.lime[300],gi
+                  width: 100.0,
+                  height: 100.0,
+                  
+                ),
+              ],
+            ),
+            // SizedBox(),
+            Container(
+              color: Colors.blue,
+              width: 100.0,
+            ),
+          ],
         ),
       ),
     );
