c761e54521cbe7bc2b3e2c3aa204bcdfc7a47007
saivishnu725 <saivishnu725@gmail.com>
2021-05-14 21:40:24 +0530

    Added two containers and stuff.

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,13 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(
-                width: 100.0,
-                height: 100.0,
+                width: 150.0,
+                height: 150.0,
               ),
-              CircleAvatar(
+              CircleAvatar(                
                 radius: 50.0,
                 backgroundImage: AssetImage('assets/avatar.png'),
-                backgroundColor: Colors.amber,
+                backgroundColor: Colors.black,
               ),
               Text(
                 'Sai Vishnu M',
@@ -39,7 +39,56 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: "Roboto Mono",
                   color: Colors.white,
-                  fontSize: 15.0,
+                  fontSize: 20.0,
+                  letterSpacing: 1.5,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(30.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 23.0,
+                    ),
+                    Text(
+                      '+91 7259255958',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 30.0,
+                  right: 30.0,
+                  bottom: 30.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 23.0,
+                    ),
+                    Text(
+                      'saivishnu725@gmail.com',
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
