8326c41b4026478d77180b456c3dc9b00c70b64f
saivishnu725 <saivishnu725@gmail.com>
2021-05-14 22:04:30 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,53 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black54,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                width: 150.0,
-                height: 150.0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('assets/avatar.png'),
+              backgroundColor: Colors.black,
+            ),
+            Text(
+              'Sai Vishnu M',
+              style: TextStyle(
+                fontFamily: 'PermanentMarker',
+                fontSize: 40.0,
+                color: Colors.red,
               ),
-              CircleAvatar(                
-                radius: 50.0,
-                backgroundImage: AssetImage('assets/avatar.png'),
-                backgroundColor: Colors.black,
-              ),
-              Text(
-                'Sai Vishnu M',
-                style: TextStyle(
-                  fontFamily: 'PermanentMarker',
-                  fontSize: 40.0,
-                  color: Colors.red,
-                ),
-              ),
-              Text(
-                'Just a tech.',
-                style: TextStyle(
-                  fontFamily: "Roboto Mono",
-                  color: Colors.white,
-                  fontSize: 20.0,
-                  letterSpacing: 1.5,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.all(30.0),
-                padding: EdgeInsets.all(10.0),
+            ),
+            Text(
+              'Just a tech.',
+              style: TextStyle(
+                fontFamily: "Roboto Mono",
                 color: Colors.white,
+                fontSize: 20.0,
+                letterSpacing: 1.5,
+              ),
+            ),
+            // Container(
+            //   margin: EdgeInsets.only(top: 8.0),
+            //   height: 2.0,
+            //   width: 160.0,
+            //   color: Colors.white54,
+            // ),
+            Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                height: 2.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white54,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(30.0),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
                 child: Row(
                   children: [
                     Icon(
@@ -65,14 +79,16 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(
-                  left: 30.0,
-                  right: 30.0,
-                  bottom: 30.0,
-                ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 30.0,
+                right: 30.0,
+                bottom: 30.0,
+              ),
+              color: Colors.white,
+              child: Padding(
                 padding: EdgeInsets.all(10.0),
-                color: Colors.white,
                 child: Row(
                   children: [
                     Icon(
@@ -91,8 +107,8 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
